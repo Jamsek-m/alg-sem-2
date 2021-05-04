52c4a24a9cf3be84f12d98ea69f7474c7d41bb16
matej <matejb96@gmail.com>
2021-05-04 17:39:52 +0200

    Popravek algoritma (sortiranje gradientov)

--- demo.py
+++ demo.py
@@ -9,25 +9,19 @@ def agent_calc_grad(agent, w):
 
 def correct_grad(grads, n, f):
     # filters and aggregates the gradients
-
-    # filter
-    norms = []
-    for gi in grads:
-        norms.append(math.sqrt(math.pow(gi[0], 2) + math.pow(gi[1], 2)))
-    norms.sort()
+    sorted_grads = sorted(grads, key=lambda g: math.sqrt(math.pow(g[0], 2) + math.pow(g[1], 2)))
     for i in range(n - f, n):  # clips f largest gradients to the value of the (f + 1)-th largest norm
-        norms[i] = norms[f + 1 - 1]
-        grads[i] = grads[f + 1 - 1]
+        sorted_grads[i] = sorted_grads[f]
 
     # aggregate
     x, y = 0, 0
-    for gi in grads:
+    for gi in sorted_grads:
         x += gi[0]
         y += gi[1]
     return x, y  # aggregated gradient
 
 
-def check_if_close_enough(g, limit=0.05):
+def check_if_close_enough(g, limit=0.5):
     # check if the estimation is good enough (aggregated gradient is small enough)
     return math.fabs(g[0]) < limit and math.fabs(g[1]) < limit
 
@@ -44,10 +38,11 @@ def draw(agents, w):
 # which on our map lies between 0 and 20 on the x axis and 0 and 30 on the y axis
 
 # arguments:
-agents = [(1, 1), (10, 0), (17, 1), (33, 138), (67, 281)]  # agents
+# agents = [(1, 1), (67, 281), (10, 0), (17, 1), (33, 138)]  # agents
+agents = [(1, 1), (12, 11), (10, 0), (17, 1), (333, 191), (1, 4), (19, 20), (233, 91)]  # agents
 f = 2  # number of the Byzantine faulty agents (f < n/2) TODO: try to detect automatically
 w = [10, 15]  # first estimation (random or something)
-step = 0.05  # constant (speed of moving on the gradient)
+step = 0.1  # constant (speed of moving on the gradient)
 
 n = len(agents)  # number of the agents
 grads = [None] * n  # gradients
